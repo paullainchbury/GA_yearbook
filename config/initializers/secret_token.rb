c94f03365b050d3f39888a91a0a3cb577296a41e
Paul Lainchbury <paul@lainchbury.com>
2014-03-12 09:52:40 +0000

    Adding files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GaYearbook::Application.config.secret_token = 'ba81d15e14e624190b6069af7e32af0e6060e87c45a19f08705c7c14d015cab735d397783ac30922bb6f19be1ced9a94480a7e533e7dca2be39038fce43acdbc'
